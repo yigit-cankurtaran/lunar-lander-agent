f986f0809e91103f3a19d7c82ae577cbbad64d9d
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-08-24 19:46:14 +0300

    longer training with GAE and gamma

--- training.py
+++ training.py
@@ -8,7 +8,7 @@ import os # creating folders and such
 def train():
     train_env = make_vec_env("LunarLander-v3", n_envs=4, seed=0)
     eval_env = Monitor(gym.make("LunarLander-v3"))
-    train_count = 1000000 # 1M
+    train_count = 3000000 # 3M
 
     os.makedirs("models", exist_ok=True)
     os.makedirs("logs", exist_ok=True)
@@ -27,6 +27,8 @@ def train():
         batch_size=128,
         n_steps=2048, # steps before model trains itself
         n_epochs=10, # how many times model trains itself from step data
+        gamma=0.99, # used calculating future vs immediate rewards
+        gae_lambda=0.95
 )
 
     model.learn(
