f079c82db44f88bad9172e587e79c14e0e94112a
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-08-29 18:17:50 +0300

    readability

--- continuous/testing.py
+++ continuous/testing.py
@@ -17,7 +17,9 @@ def test_agent(model_path="models/best_model.zip", vec_normalize_path = "models/
     
     model = PPO.load(model_path, env=env) # linking model to env
 
-    rewards, lengths = evaluate_policy(model, env, n_eval_episodes=episodes, deterministic=True, render=True, return_episode_rewards=True)
+    rewards, lengths = evaluate_policy(model, env, n_eval_episodes=episodes,
+                                       deterministic=True, render=True,
+                                       return_episode_rewards=True)
     for i in range(episodes):
         print(f"episode:{i+1}, reward:{rewards[i]}, length:{lengths[i]}")
 
